5a293185012eeb4f4be256a2632b449ab7cb386d
rafa <rbragar19@gmail.com>
2026-08-20 17:35:36 -0300

    Implementa remoção de contato

--- agenda.py
+++ agenda.py
@@ -35,7 +35,14 @@ def buscar_contato():
 
 
 def remover_contato():
-    pass
+    def remover_contato():
+    nome = input("Digite o nome exato do contato a remover: ")
+    for contato in contatos:
+        if contato["nome"].lower() == nome.lower():
+            contatos.remove(contato)
+            print("Contato removido.")
+            return
+    print("Contato não encontrado.")
 
 
 while True:
